2aede972bd341ba160d1f4793576704410808a95
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2023-09-03 23:45:08 +0500

    Added extra fields and changes to transaction migration

--- database/migrations/2023_09_03_114259_create_transactions_table.php
+++ database/migrations/2023_09_03_114259_create_transactions_table.php
@@ -13,17 +13,23 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->ulid('ulid')->unique(); // Transaction ID
 
-            $table->unsignedMediumInteger('amount');
-            $table->unsignedMediumInteger('received')->default(0);
+            $table->unsignedMediumInteger('amount'); // Amount requested
+            $table->unsignedMediumInteger('received')->default(0); // Amount received
+            $table->unsignedMediumInteger('fee')->default(0); // Stripe TAX
 
-            $table->string('session')->unique();
-            $table->string('secret');
+            $table->string('session')->unique(); // Stripe Checkout session ID
 
-            $table->enum('status', ['pending', 'unpaid', 'cancelled'])->default('pending');
+            $table->enum('status', ['pending', 'successful', 'cancelled'])->default('pending');
             $table->json('metadata')->nullable();
 
             $table->foreignId('user_id')->constrained();
+            $table->foreignUlid('package_id')->constrained();
+
+            // When this Stripe checkout session will expire, this will be
+            // provided after the checkout session has been created
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
